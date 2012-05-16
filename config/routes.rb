d90799dbf71e69882425850fe04d2b1ce99f1d8b
Jack Bishop <jack.bishop@synctree.com>
2012-05-16 13:44:51 -0700

    Worked with basic generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Kookoo::Application.routes.draw do
+  get "users/new"
+
+  get "users/edit"
+
+  get "users/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
