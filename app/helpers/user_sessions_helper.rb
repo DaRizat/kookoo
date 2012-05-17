6261a4de6709e7e4096eb0ecf79cace88e2f64af
Jack Bishop <jack.bishop@synctree.com>
2012-05-16 21:28:20 -0700

    Authentication totally complete, bootstrap added

---
+++ app/helpers/user_sessions_helper.rb
@@ -0,0 +1,7 @@
+module UserSessionsHelper
+
+  def user_logged_in?
+    not session[:user_id].blank?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Kookoo::Application.routes.draw do
 
   resources :users
-
+  resources :user_sessions
+  
+  get "log_out" => "user_sessions#destroy", :as => "log_out"
+  get "log_in" => "user_sessions#new", :as => "log_in"
+  
   root :to => "users#new" 
 
   # The priority is based upon order of creation:
